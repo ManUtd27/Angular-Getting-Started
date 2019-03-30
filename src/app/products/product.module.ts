9ab0ebf480fc881f7b3a4d3aac9613c28a492b29
Shawn Williams <jr.william09@gmail.com>
2019-03-30 15:05:20 -0500

    add child routes for edit product tabs with cross child route validation

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { ProductListComponent } from './product-list.component';
-import { ProductDetailComponent } from './product-detail.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
+import {ProductListComponent} from './product-list.component';
+import {ProductDetailComponent} from './product-detail.component';
+import {ProductEditComponent} from './product-edit/product-edit.component';
 
-import { SharedModule } from '../shared/shared.module';
+import {SharedModule} from '../shared/shared.module';
 import {ProductsRoutingModule} from './products-routing.module';
+import {ProductEditInfoComponent} from './product-edit/product-edit-info.component';
+import {ProductEditTagsComponent} from './product-edit/product-edit-tags.component';
 
 @NgModule({
   imports: [
@@ -15,7 +17,10 @@ import {ProductsRoutingModule} from './products-routing.module';
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
-export class ProductModule { }
+export class ProductModule {
+}
